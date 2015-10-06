a1c2fcd2261195beeb6873cc776daa1ba2ee4942
Toby Rumans <trumans@pivotal.io>
2015-10-06 16:17:12 -0700

    add yeller sink to jettison
    [#104811016]
    
    Signed-off-by: Noah Denton <ndenton@pivotal.io>

--- cmd/jettison/main.go
+++ cmd/jettison/main.go
@@ -9,6 +9,7 @@ import (
 	gconn "github.com/cloudfoundry-incubator/garden/client/connection"
 	"github.com/concourse/jettison"
 	"github.com/pivotal-golang/lager"
+	"github.com/xoebus/zest"
 )
 
 var gardenAddr = flag.String(
@@ -17,11 +18,27 @@ var gardenAddr = flag.String(
 	"garden API host:port",
 )
 
+var yellerAPIKey = flag.String(
+	"yellerAPIKey",
+	"",
+	"API token to output error logs to Yeller",
+)
+var yellerEnvironment = flag.String(
+	"yellerEnvironment",
+	"development",
+	"environment label for Yeller",
+)
+
 func main() {
 	flag.Parse()
 
 	logger := lager.NewLogger("jettison")
 	logger.RegisterSink(lager.NewWriterSink(os.Stdout, lager.DEBUG))
+
+	if *yellerAPIKey != "" {
+		yellerSink := zest.NewYellerSink(*yellerAPIKey, *yellerEnvironment)
+		logger.RegisterSink(yellerSink)
+	}
 
 	gardenClient := gclient.New(gconn.New("tcp", *gardenAddr))
 
